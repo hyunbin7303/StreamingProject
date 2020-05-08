ff4719d10c0d4ca242c00d881e67fa76fff7b363
Hyunbin Park <hyunbin7303@gmail.com>
2020-05-07 23:59:33 -0400

    Structure change

---
+++ UDP_Streaming/Common/inc/common.h
@@ -0,0 +1,56 @@
+
+
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <ctype.h>
+#include <iostream>
+#define SOCKET_ERROR -1
+#define INVALID_SOCKET -1
+#define ERROR 0
+#define FIRST_CONNECTION_FAIL -10
+#define SIZEBUF 22000
+#define LINUX_SERVER 1
+#define LINUX_CLIENT 2
+#define FAIL_WINDOW -1
+#define FAIL_LINUX -2
+
+#define SERVER_ARG 3
+#define CLIENT_ARG 10
+#define INVALID_CMD_ARG -1
+#define UDP 20
+#define TIMEOUT_MS 
+#define SERVICE_DONE 		100
+#define SERVICE_RESTART 	200
+#define SERVICE_ERROR 		-100
+
+typedef struct
+{
+	int Protocol;
+	char* IpAddr;
+	int portNum;
+	int numBlock;
+	int BlockSize;
+}CommandLineValues;
+
+CommandLineValues EachValue;
+char* ipAdd;
+int commandLineParser(int argc, char** argv);
+int isNumberCheck(char* str);
+
+
+#endif
---
+++ UDP_Streaming/Server/src/server.cpp
@@ -0,0 +1,15 @@
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+#include <iostream>
+
+
+int testing()
+{
+
+	std::cout <<"HAHAHAHAHA" << std::endl;
+	return 20;
+}
+
+
+#endif
